4ada1085a5518f21ece437d130c0158402dc58c6
mits <mits.jp@gmail.com>
2014-01-21 00:37:02 +0900

    演習１３−１　完了

--- IS-prac13/ViewController.m
+++ IS-prac13/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+- (void) hello:(UIButton *)sender;
+
 @end
 
 @implementation ViewController
@@ -18,12 +20,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button.frame = CGRectMake(50, 50, 100, 40);
+    [self.view addSubview:button];
+    
+    [button addTarget:self action:@selector(hello:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) hello:(UIButton *)sender
+{
+    [[[UIAlertView alloc] initWithTitle:@"Button" message:@"クリック" delegate:self cancelButtonTitle:@"キャンセル" otherButtonTitles:@"OK", nil] show];
 }
 
 @end
